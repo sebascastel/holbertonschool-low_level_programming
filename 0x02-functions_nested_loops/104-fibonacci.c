36d617081f6d40e136fa9d5cd6c1dcd77cde7909
sebascastel <770@holbertonschool.com>
2019-02-11 21:33:11 +0000

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	long int i, f, s, n, h2, h1, h3, h4, h0, h5;
+
+	f = 0;
+	s = 1;
+	i = 0;
+	while (i < 91)
+	{
+		n = f + s;
+		f = s;
+		s = n;
+		printf("%ld", n);
+		printf(",");
+		printf(" ");
+		i++;
+	}
+
+	h4 = f / 1000000000;
+	h3 = f % 1000000000;
+	h2 = s / 1000000000;
+	h1 = s % 1000000000;
+	while (i < 97)
+	{
+		h0 = h4 + h2;
+		h5 = h3 + h1;
+		printf("%ld""%ld", h0, h5);
+		if (i != 96)
+		{
+			printf(",");
+			printf(" ");
+		}
+		h4 = h2;
+		h3 = h1;
+		h2 = h0;
+		h1 = h5;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
