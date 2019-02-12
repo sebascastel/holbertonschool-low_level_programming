891c0305ff287abc7c9fbafb7f8e1b38e16b61bc
sebascastel <770@holbertonschool.com>
2019-02-12 14:45:45 +0000

    using prototype

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,92 +1,49 @@
 #include "holberton.h"
 /**
- * print_digits - Entry point
- *@a: check
- *@b: check
- * Return: (0) always
- */
-
-void print_digits(int a, int b)
-{
-	if ((a * b) > 99)
-	{
-		_putchar(((a * b) / 100) + '0');
-		_putchar((((a * b) / 10) % 10) + '0');
-		_putchar(((a * b) % 10) + '0');
-	}
-	else if (((a * b) < 100) && ((a * b) > 9))
-	{
-		_putchar(((a * b) / 10) + '0');
-		_putchar(((a * b) % 10) + '0');
-	}
-	else
-	{
-		_putchar(((a * b) % 10) + '0');
-	}
-
-}
-/**
- * print_spaces - helper function, prints out number of spaces based on x * y
- * @m: rows
- * @n: column
- * Return: void
- */
-void print_spaces(int m, int n)
-{
-
-	if ((m * n) >= 100)
-	{
-		_putchar(' ');
-	}
-	else if ((m * n) < 100 && (m * n) > 9)
-	{
-		_putchar(' ');
-		_putchar(' ');
-	}
-	else if ((m * n) < 10)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-	}
-
-}
-
-/**
- * print_times_table - prints multiplication tables between 1 and 15
- * @n: multiplication
- * Return: void
+ * print_times_table - prints the multiplicaiton times table
+ * @n: takes in the what times table user wants, type int
+ * Prints only digts and uses _putchar function
  */
 void print_times_table(int n)
 {
+	int c, r, num, tens;
 
-	int x, y;
-
-	if (n < 0 || n > 15)
+	if (!(n >= 0 && n < 15))
 		return;
-
-	x = 0;
-
-	while (x <= n)
+	for (r = 0; r <= n; r++)
 	{
-		y = 0;
-
-		while (y <= n)
+		for (c = 0; c <= n ; c++)
 		{
-			print_digits(x, y);
-
-			if (y != n)
-				_putchar(',');
-			else
-			{
-				_putchar('\n');
-				break;
+			num = r * c;
+			tens = num / 10;
+			if (tens > 9) /* makes sure tens is a single digit ex. 123/10=12%10=2*/
+				tens = tens % 10;
+			if (c == 0) /*Print the first coloum*/
+				_putchar('0');
+			else if (tens == 0 && (num / 100) == 0)
+			{/*print only single digits and so 101 work*/
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((num % 10) + '0');
 			}
-			++y;
+			else if ((num / 100) == 0) /*Print double digits*/
+			{
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar((num % 10) + '0');
 
-			print_spaces(x, y);
-		}
-		++x;
+			}
+			else /*print everything else*/
+			{
+				_putchar((num / 100) + '0');
+				_putchar(tens + '0');
+				_putchar((num % 10) + '0');
+			}
+			if (c != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		} _putchar('\n');
 	}
-
 }
