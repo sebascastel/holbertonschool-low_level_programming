49c2cbdb2b2cafd19165d3007337a3d11becdc7e
sebascastel <770@holbertonschool.com>
2019-02-18 17:43:55 +0000

    stringprint

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts - string.
+ * @str : string
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+	_putchar(str[n]);
+	n++;
+	}
+	_putchar (10);
+}
