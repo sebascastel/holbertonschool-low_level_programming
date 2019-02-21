635e8fee3f7ac39cff9662eb6d69af09841c69d9
sebascastel <770@holbertonschool.com>
2019-02-21 14:50:20 +0000

    concadenation

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_strcat - concatenates 2 strings.
+ * @dest : str1
+ * @src : str2
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	a++;
+	while (src[b] != '\0')
+	{
+	dest[a+b] = src[b];
+	b++;
+	}
+	return (dest);
+}
