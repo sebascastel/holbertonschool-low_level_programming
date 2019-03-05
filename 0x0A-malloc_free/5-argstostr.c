d5a7d45693a9b76cdff3611cb2952576cde358c2
sebascastel <770@holbertonschool.com>
2019-03-05 16:14:40 +0000

    malloc - free

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+/** *argstostr - concatenates all the arguments
+ *@ac: count arguments 
+ *@av: arguments
+ *@return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e = 0;
+	int f = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+	for (b = 0; av[a][b] != '\0'; b++)
+	e++;
+	e++;
+	}
+	s = (char*)malloc(sizeof(char) * (e + 1));
+	if (s == NULL)
+	return (NULL);
+	for (c = 0; c < ac; c++ , f++)
+	{
+	for (d = 0; av[c][d]; d++, f++)
+	{
+	s[f] = av [c][d];
+	}
+	s[f] = '\n';
+	}
+	s[f] = '\0';
+	return (s);
+}
