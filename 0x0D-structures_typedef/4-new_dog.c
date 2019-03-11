53f87ec0928400442ab45507ef550aa2153ab23a
sebascastel <770@holbertonschool.com>
2019-03-11 16:47:43 +0000

    new dog

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+char *_strdup(char *str);
+/**
+ **new_dog - initialize a variable of type struct dog
+ *@name: name dogo
+ *@age: age dogo
+ *@owner: dogo owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogo;
+
+	if (name == NULL || owner == NULL)
+	return (NULL);
+	dogo = malloc(sizeof(dog_t));
+	if (dogo == NULL)
+	return (NULL);
+	dogo->name = _strdup(name);
+	if (dogo->name == NULL)
+	{
+	free(dogo);
+	return (NULL);
+	}
+	dogo->owner = _strdup(owner);
+	if (dogo->owner == NULL)
+	{
+	free(dogo->name);
+	free(dogo);
+	return (NULL);
+	}
+	dogo->age = age;
+	return (dogo);
+}
+/**
+ * *_strdup - turns a pointer to a newly allocated space in memory
+ *@str: char
+ * Return: (0) always
+ */
+char *_strdup(char *str)
+{
+	int a;
+	int b = 0;
+	char *s;
+
+	if (str == NULL)
+	return (NULL);
+	while (str[b] != '\0')
+	b++;
+	s = malloc(sizeof(char) * (b + 1));
+	if (s == NULL)
+	return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+	s[a] = str[a];
+	s[a] = '\0';
+	return (s);
+}
