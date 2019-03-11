5e609fe3af76436783df2c9480a8caac32de9cc8
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-11 18:23:55 -0500

    atoi toi

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,25 @@
 #include "holberton.h"
 /**
  * _atoi - Converts a array to an integer
- *
-
-
-
-
  * @s: character array to convert
  * Return: 0
  */
 int _atoi(char *s)
 {
 	int a:
-  int b;
-  int c;
+	int b;
+	int c;
 
-	 = 0;
-	= 0;
-	 = -1;
-	while (s[b] != '\0' && (s[b] < '0' || s[b] > '9'))
+	a = 0;
+	b = 0;
+	c = -1;
+	while (s[b] != '\0' && (s[b] < 48 || s[b] > 57))
 	{
-		if (s[i] == '-')
-			 *= -1;
-		++;
+	if (s[b] == '-')
+	c *= -1;
+	b ++;
 	}
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-		 = (a * 10) - (s[a++] - '0');
+	while (s[b] != '\0' && (s[b] >= 48 && s[b] <= 57))
+	a = (a * 10) - (s[a++] - '0');
 	return (a * c);
 }
