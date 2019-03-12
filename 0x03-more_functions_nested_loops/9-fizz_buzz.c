f5079a1569041e227eaa3579008a0c83769cc1ec
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-11 19:51:21 -0500

    fixed fiz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,24 @@
 
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 1; n <= 100; n++)
+	for (a = 1; a <= 100; a++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
-		printf("FizzBuzz");
-		else if (n % 5 == 0)
-		printf("Buzz");
-		else if (n % 3 == 0)
-		printf("Fizz");
-		else
-		printf("%d", n);
-		if (n != 100)
-		printf(" ");
+	if (a % 3 == 0)
+	{
+	if (a % 5 == 0)
+	printf("FizzBuzz ");
+	else
+	printf("Fizz ");
+	}
+	else if (a % 5 == 0)
+	if (a == 100)
+	printf("Buzz");
+	else
+	printf("Buzz ");
+	else
+	printf("%d ", a);
 	}
 	printf("\n");
 	return (0);
