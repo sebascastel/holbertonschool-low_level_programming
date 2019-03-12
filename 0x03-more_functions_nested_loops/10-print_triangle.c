6c86b4b3216ea8ee9b11783519968b602dac383d
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-11 19:47:44 -0500

    fixed triangle

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,23 +1,32 @@
 #include "holberton.h"
 /**
- * print_triangle - check the code for Holberton School students.
- * @n : check
+ * print_triangle - prints a triangle.
+ * @size: size
  * Return: Always 0.
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int a; 
+	int b;
+	int n;
 
-	j = 0;
-		for (i = 0 ; i < size ; i++)
-		{
-			if (size > 0)
-				for(j = i ; j >= 0 ; j--)
-					_putchar (' ');
-					_putchar ('#');
-			_putchar (10);
+	a = size;
+	for (n = 0; n < size; n++)
+	{
+	b = n;
+	a = size - b;
+	while (a > 1)
+	{
+	_putchar(32);
+	a--;
 	}
-		if (size <= 0)
-			_putchar (10);
-
+	while (b >= 0)
+	{
+	_putchar('#');
+	b--;
+	}
+	_putchar(10);
+	}
+	if (size <= 0)
+	_putchar(10);
 }
