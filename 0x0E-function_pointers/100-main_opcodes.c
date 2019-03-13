4fd42934a76957fb8cf263e2230d91a9b5b20482
sebascastel <770@holbertonschool.com>
2019-03-13 15:59:10 +0000

    file

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - start point
+ *@argc : count
+ *@argv : vec
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+	printf("Error\n");
+	exit(1);
+	}
+}
