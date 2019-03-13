dfb5a850bc4d2c6d8f278acb1b8638b26ca3c05a
sebascastel <770@holbertonschool.com>
2019-03-13 16:04:31 +0000

    exit,error conditions

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -8,9 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int a;
+
 	if (argc != 2)
 	{
 	printf("Error\n");
 	exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+	printf("Error\n");
+	exit(2);
 	}
 }
