e97d9702ca5bf7d119aafa18c8373c8b3f023769
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-16 12:48:32 -0500

    Update 100-hello_holberton.asm

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,16 @@
+section .text
 global main
+section .data
+message db "Hello, holberton",0xa
+len equ $ - message
 section .text
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+	mov edx,len
+	mov ecx,message
+	mov ebx,1
+	mov eax,4
+	int 0x80
 
-	mov eax,60
-	xor rdi, rdi
-	syscall
-msg:
-db "Hello, holberton", 10
+	mov ebx,0
+	mov eax,1 
+	int 0x80
