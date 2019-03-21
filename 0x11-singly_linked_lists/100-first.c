7428faac46462e45aad98cfd320ae964b68b77fc
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-21 17:12:37 -0500

    Create 100-first.c

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+*print_before - print before the main
+*/
+void print_before(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
