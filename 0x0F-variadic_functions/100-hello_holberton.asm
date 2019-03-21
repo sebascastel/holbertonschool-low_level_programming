5f741a6c42000ac5b65a799676f7de7955bb2ee8
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-03-21 16:12:19 -0500

    Update 100-hello_holberton.asm

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,27 @@
-section .text
-global main
-section .data
-message db "Hello, holberton",0xa
-len equ $ - message
-section .text
-main:
-	mov edx,len
-	mov ecx,message
-	mov ebx,1
-	mov eax,4
-	int 0x80
+section     .text
+global      main	;must be declared for linker (ld)
+main:	mov	rax, 1			;tell linker entry point
+	mov     rdi, 1	;message length
+	mov     rsi, msg	;message to write
+	mov     rdx,17	;system call number
+	syscall
+	mov	    rax, 60	;call kernel
+	xor     rdi, rdi	;system call number (sys_exit)
+	syscall
+section     .data
+msg:       db  "Hello, Holberton", 10, 0	;our dear string
 
-	mov ebx,0
-	mov eax,1 
-	int 0x80
+
+
+
+
+
+
+
+
+
+
+
+
+
+
